a7d6da4ca12fc570a3e11233a022f7b152cc120c
colmassakian <43759428+colmassakian@users.noreply.github.com>
2019-12-25 12:26:57 -0800

    Added start button

--- src/main/java/AStar.java
+++ src/main/java/AStar.java
@@ -23,13 +23,14 @@ public class AStar {
                 JFrame frame = new JFrame("AStar");
                 frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
                 frame.setLayout(new BorderLayout());
-                frame.add(new AStarPane());
+                AStarPane starPane = new AStarPane();
+                frame.add(starPane);
                 frame.setPreferredSize(new Dimension(1600, 1400));
                 frame.pack();
                 frame.setLocationRelativeTo(null);
                 frame.addKeyListener(new KeyListener() {
                     public void keyPressed(KeyEvent e) {
-                        if(e.getKeyCode() ==KeyEvent.VK_B)
+                        if(e.getKeyCode() == KeyEvent.VK_B)
                             makeBorder = !makeBorder;
                     }
 
@@ -37,6 +38,18 @@ public class AStar {
 
                     public void keyTyped(KeyEvent e) {}
                 });
+//                JButton setStartPoint = new JButton("Set Start Point");
+//                JButton setEndPoint = new JButton("Set End Point");
+//                JButton selector = new JButton("Dijkstra/AStar");
+                JButton start = new JButton("START!");
+                start.addActionListener(e -> starPane.startPathFinder());
+                JPanel panel1 = new JPanel();
+//                panel1.add(setStartPoint);
+//                panel1.add(setEndPoint);
+//                panel1.add(selector);
+                panel1.add(start);
+                frame.add(panel1, BorderLayout.NORTH);  // add the panel to the frame
+                frame.setFocusable(true);
                 frame.setVisible(true);
 
             }
